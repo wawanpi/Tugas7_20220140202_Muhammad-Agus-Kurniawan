165bb7f656d64ca1dbdbd133ea63269aa5781858
wawanpi <muhaguskurniawan@gmail.com>
2024-04-07 01:48:30 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,12 @@
+video.addEventListener("click", function video() {
+    alert("Ini Video");
+})
+
+audio.addEventListener("click", function audio() {
+    alert("Ini Audio");
+})
+
+gambar.addEventListener("click", function gambar() {
+    alert("Ini Gambar");
+    
+})
